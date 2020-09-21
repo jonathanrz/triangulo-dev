231c0772943897a47dc484b13674d3b918fa83ef
Hugo Bessa <eu@hugobessa.com.br>
2020-09-21 11:16:10 -0300

    Implement post content loading

--- triangulo-dev/pages/posts/[slug].tsx
+++ triangulo-dev/pages/posts/[slug].tsx
@@ -1,9 +1,11 @@
+import jetpack from "fs-jetpack";
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import path from "path";
 
-function Post({ post }: InferGetStaticPropsType<typeof getStaticProps>) {
+function Post({ postContent }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div>
-      <pre>{post}</pre>
+      <pre>{postContent}</pre>
     </div>
   );
 }
@@ -12,8 +14,11 @@ export let getStaticProps: GetStaticProps = async function (context) {
   let slug = context.params?.slug;
 
   if (slug) {
-    let post = await import(`../../content/posts/${slug}/index.md`);
-    return { props: { post } };
+    let postsDirectory = path.join(process.cwd(), "content", "posts");
+    let filePath = path.join(postsDirectory, `${slug}/index.md`);
+
+    let postContent = await jetpack.readAsync(filePath);
+    return { props: { postContent } };
   }
 
   return { props: {} };
