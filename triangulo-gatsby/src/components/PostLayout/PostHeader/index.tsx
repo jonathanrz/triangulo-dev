a9537a5c3a312a04a4b25b2d2860138a593218c0
Hugo Bessa <eu@hugobessa.com.br>
2020-10-01 19:06:26 -0300

    Add more MDX components

--- triangulo-gatsby/src/components/PostLayout/PostHeader/index.tsx
+++ triangulo-gatsby/src/components/PostLayout/PostHeader/index.tsx
@@ -12,11 +12,11 @@ let PostHeader: React.FC<PostHeaderProps> = ({ frontmatter }) => {
   return (
     <Box as="header" maxWidth="4xl" mx="auto" py="12">
       <Heading as="h1" fontSize="4xl">
-        {frontmatter?.title}
+        {frontmatter.title}
       </Heading>
       <Stack isInline={true} spacing="2" alignItems="center" mt="2">
         <Box borderRadius="50%" w="30px" h="30px" bg="green.500" />
-        <Text>por {frontmatter?.author || "Hugo Bessa"}</Text>
+        <Text>por {frontmatter.author || "Hugo Bessa"}</Text>
       </Stack>
     </Box>
   )
--- triangulo-gatsby/src/config/theme.ts
+++ triangulo-gatsby/src/config/theme.ts
@@ -1,9 +1,5 @@
-import { theme } from "@chakra-ui/core";
+import { theme } from "@chakra-ui/core"
 
 export default {
   ...theme,
-  fonts: {
-    body: "'Inter', system-ui, sans-serif",
-    heading: "'Inter', system-ui, sans-serif",
-  },
-};
+}
