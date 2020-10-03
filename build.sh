9f2a0a7283ad140d3194373aefcd9745ed35e6d3
Hugo Bessa <eu@hugobessa.com.br>
2020-10-03 12:11:30 -0300

    Replace older website by NextJS's

--- build.sh
+++
@@ -1,6 +0,0 @@
-curl -L -O https://github.com/gohugoio/hugo/releases/download/v0.55.6/hugo_0.55.6_Linux-64bit.tar.gz
-tar -xzf hugo_0.55.6_Linux-64bit.tar.gz
-
-mkdir -p ./tmp
-echo "baseURL = \"https://triangulo.dev/\"" >> ./tmp/config.toml
-./hugo --config config.toml,./tmp/config.toml
--- triangulo-dev/README.md
+++
@@ -1,9 +0,0 @@
-# triangulo.dev
-
-Oh hey. This is just starting.
-
-## Features
-
-- Posts
-- Downloadables
-- Courses
