4c90f20f65b64d5fd78f0843e6e1e0f3804203e4
Hugo Bessa <eu@hugobessa.com.br>
2020-10-03 11:37:48 -0300

    Move to tsx, add SEO to App

--- triangulo-dev/src/config/theme.ts
+++ triangulo-dev/src/config/theme.ts
@@ -3,6 +3,7 @@ import { theme } from "@chakra-ui/core";
 export default {
   ...theme,
   fonts: {
+    ...theme.fonts,
     body: "'Inter', system-ui, sans-serif",
     heading: "'Inter', system-ui, sans-serif",
   },
--- triangulo-dev/src/pages/_app.tsx
+++ triangulo-dev/src/pages/_app.tsx
@@ -1,19 +1,24 @@
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import theme from "../config/theme";
 import Head from "next/head";
+import SEO from "@/components/SEO";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
+    <>
+      <SEO />
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
-      <CSSReset />
-      <Component {...pageProps} />
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
