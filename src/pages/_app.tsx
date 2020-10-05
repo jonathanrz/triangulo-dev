db1bde14f5b8b1d0de7a7d9761b884bb9b8b0b8b
Hugo Bessa <eu@hugobessa.com.br>
2020-10-05 12:13:16 -0300

    Add Google Analytics tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,22 @@ function MyApp({ Component, pageProps }: AppProps) {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
         />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-179589724-1"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-179589724-1');
+            `,
+          }}
+        />
       </Head>
       <ThemeProvider theme={theme}>
         <CSSReset />
