e298c337fecfe022f7fb30bc448647e5c7c14ae1
Hugo Bessa <eu@hugobessa.com.br>
2020-10-05 12:20:06 -0300

    Add Twitter pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,6 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           async
           src="https://www.googletagmanager.com/gtag/js?id=UA-179589724-1"
         ></script>
+
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -29,6 +30,21 @@ function MyApp({ Component, pageProps }: AppProps) {
             `,
           }}
         />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+              },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+              a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+              // Insert Twitter Pixel ID and Standard Event data below
+              twq('init','o4pbn');
+              twq('track','PageView');
+            `,
+          }}
+        />
+
+        <script></script>
       </Head>
       <ThemeProvider theme={theme}>
         <CSSReset />
